7378013f41d8a6d74e9d134c5e7df72f27927fd7
cposada23 <cposadaa@gmail.com>
2015-09-13 12:49:10 -0500

    Funciona los if, busca solo por apellido o por nombre

--- src/java/com/udea/controller/BuscarServlet.java
+++ src/java/com/udea/controller/BuscarServlet.java
@@ -5,6 +5,7 @@
  */
 package com.udea.controller;
 
+import static com.sun.corba.se.impl.util.Utility.printStackTrace;
 import com.udea.modelo.JugadorDTO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
@@ -58,10 +59,12 @@ public class BuscarServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         String nombre = request.getParameter("nombre");
         String apellido = request.getParameter("lastName");
-
+        System.out.println(nombre);
+        System.out.println(nombre.length());
+        System.out.println(apellido.length());
         Blob blob = null;
         byte[] data = null;
-
+        ResultSet rs = null;
         //Defino el input stream para el archivo que subire
         OutputStream outputStream = null;
         //InputStream inputStream=null;
@@ -80,7 +83,7 @@ public class BuscarServlet extends HttpServlet {
             PreparedStatement statement = null;
 
 
-            if (nombre != "" && apellido != "") {
+            if (nombre.length()>1&& apellido.length()>1) {
                 //construyo el estamento SQL
                 String sql = "SELECT  first_name,last_name,edad,posicion,equipo,fecha_nacimiento,nacionalidad,photo FROM jugador WHERE first_name=? AND last_name=?";
                 //Crei el prepareStatement SQL para enlazarlo con el POJO
@@ -88,24 +91,28 @@ public class BuscarServlet extends HttpServlet {
 
                 statement.setString(1, nombre);
                 statement.setString(2, apellido);
-            } else if (nombre == "") {
+                System.out.println("entra al if nombre y apellidos no nulos");
+                
+            } else if (apellido.length()>1) {
                 //construyo el estamento SQL
                 String sql = "SELECT  first_name,last_name,edad,posicion,equipo,fecha_nacimiento,nacionalidad,photo FROM jugador WHERE last_name=?";
                 //Crei el prepareStatement SQL para enlazarlo con el POJO
                 statement = conn.prepareStatement(sql);
 
                 statement.setString(1, apellido);
+                System.out.println("entro al else apellido nulo");
+                
             } else {
                 //construyo el estamento SQL
                 String sql = "SELECT  first_name,last_name,edad,posicion,equipo,fecha_nacimiento,nacionalidad,photo FROM jugador WHERE first_name=?";
                 //Crei el prepareStatement SQL para enlazarlo con el POJO
                 statement = conn.prepareStatement(sql);
-
                 statement.setString(1, nombre);
+                System.out.println("entro al else nombre nulo");
             }
 
             //enviar el stamento para actualizar la BD  
-            ResultSet rs = statement.executeQuery();
+            rs = statement.executeQuery();
             while (rs.next()) {
                 jug.setFirstName(rs.getString("first_name"));
                 jug.setLastName(rs.getString("last_name"));
@@ -149,6 +156,7 @@ public class BuscarServlet extends HttpServlet {
             request.setAttribute("fecha_nacimiento", jug.getFechaNam());
             request.setAttribute("nacionalidad", jug.getNacionalidad());
             request.setAttribute("Message", message);
+            
             //reenvio a la vista jsp del mensaje
             getServletContext().getRequestDispatcher("/resultado.jsp").forward(request, response);
         }
